58f69ffdd0731d6fd02d73469ac423048c25b62e
bvr <bvr@c770f1bc-0775-48ed-bd74-6edc8452e29f>
2010-02-05 23:01:47 +0000

    Organizing and refining NNforZR codes. Also added example matplotlib code for personal reference.
    git-svn-id: svn+ssh://localhost/home/bvr/svnrepos/Programs/NNforZR@99 c770f1bc-0775-48ed-bd74-6edc8452e29f

--- DoNNZRBootstrapping.m
+++ DoNNZRBootstrapping.m
@@ -1,48 +1,41 @@
-function DoNNZRBootstrapping()
+function DoNNZRBootstrapping(dirLoc)
 
-dirLoc = 'ModelProject_Retry7/';
-models = {'FullSet', 'SansWind', 'JustWind', 'Reflect', 'ZRBest'};
-statNames = {'corr', 'rmse', 'mae'};
-statNamesFull = {'Correlation Coefficient', 'Root Mean Squared Error [mm/hr]', 'Mean Absolute Error [mm/hr]'};
-statNamesTitle = {'Correlations', 'RMSEs', 'MAEs'};
+    models = {'FullSet', 'SansWind', 'JustWind', 'Reflect', 'ZRBest'};
+    statNames = {'corr', 'rmse', 'mae'};
+    statNamesFull = {'Correlation Coefficient', 'Root Mean Squared Error [mm/hr]', 'Mean Absolute Error [mm/hr]'};
+    statNamesTitle = {'Correlations', 'RMSEs', 'MAEs'};
 
-for statIndex = 1:length(statNames)
-    bootMeans = zeros(length(models), 1);
-    bootCIs = zeros(length(models), 2);
+    for statIndex = 1:length(statNames)
+        bootMeans = zeros(length(models), 1);
+        bootCIs = zeros(length(models), 2);
 
-    for modelIndex = 1:length(models)
-        [bootMeans(modelIndex), bootCIs(modelIndex, :)] = ProcessFile(fullfile(dirLoc, ['summary_' statNames{statIndex} ...
-                                                                                        '_' models{modelIndex} '.txt']));
-
-    end
+        for modelIndex = 1:length(models)
+            [bootMeans(modelIndex), bootCIs(modelIndex, :)] = ProcessFile(fullfile(dirLoc, ['summary_' statNames{statIndex} ...
+                                                                                            '_' models{modelIndex} '.txt']));
+        end
     
-    %h = axes('Position',[0 0 1 1],'Visible','off');
-    %axes('Position',[.1 .1 .8 .8]);
-    disp(bootCIs(:, 1));
-    disp(bootMeans);
-    disp(bootCIs(:, 2));
+        %h = axes('Position',[0 0 1 1],'Visible','off');
+        %axes('Position',[.1 .1 .8 .8]);
+        disp(bootCIs(:, 1));
+        disp(bootMeans);
+        disp(bootCIs(:, 2));
     
-    figure;
-    errorbar(1:length(models), bootMeans, bootMeans - bootCIs(:, 1), ...
-                                          bootCIs(:, 2) - bootMeans, ...
-                               '.k', 'LineWidth', 1.5, 'MarkerSize', 20);
+        figure;
+        errorbar(1:length(models), bootMeans, bootMeans - bootCIs(:, 1), ...
+                                              bootCIs(:, 2) - bootMeans, ...
+                                   '.k', 'LineWidth', 1.5, 'MarkerSize', 20);
  
-    set(gca, 'XTick', 1:length(models), 'XTickLabel', models);
-    ylabel(statNamesFull{statIndex}, 'FontSize', 12);
-    xlabel('Models', 'FontSize', 12);
-    title(['Mean Model ' statNamesTitle{statIndex}], 'FontSize', 13);
-
-end
+        set(gca, 'XTick', 1:length(models), 'XTickLabel', models);
+        ylabel(statNamesFull{statIndex}, 'FontSize', 12);
+        xlabel('Models', 'FontSize', 12);
+        title(['Mean Model ' statNamesTitle{statIndex}], 'FontSize', 13);
+    end
                                                                            
-
-%ProjectAnalyze <- function(dirLoc, statName, subProjs, titleName, compareToIndex)
-
-
 
 
 function [statBoot, statCI] = ProcessFile(fileName)
 
-C = load(fileName, '-ascii');
+    C = load(fileName, '-ascii');
 
-statBoot = mean(bootstrp(2000, @mean, C));
-statCI = bootci(2000, {@mean, C}, 'alpha', 0.1, 'type', 'bca');    % does the 90 percentile interval using BCa
+    statBoot = mean(bootstrp(2000, @mean, C));
+    statCI = bootci(2000, {@mean, C}, 'alpha', 0.1, 'type', 'bca');    % does the 90 percentile interval using BCa
--- PaperScript.py
+++ PaperScript.py
@@ -1,201 +1,53 @@
 #!/usr/bin/env python
 
-
-from optparse import OptionParser	# Command-line parsing
-
 import glob			# for filename globbing
-import os
-import numpy
-import scipy.stats as ss	# for sem() and other stat funcs
-import scipy.stats.stats as sss	# for nanmean() and other nan-friendly funcs
-import pylab			# for plotting
+import os			# for os.sep
 
-from filtertraining import *	# for MakeBins(), Hist2d()
+import pylab
 
-import matplotlib		# for colormap
-
-from scipy import optimize
-#from arff import arffread
-
-def ZRModel(coefs, reflects) :
-    return(((10.0 **(reflects/10.0))/coefs[0]) ** (1/coefs[1]))
-
-def ZRBest(trainData) :
-    def errFun(coefs) :
-        return(numpy.sqrt(numpy.mean((ZRModel(coefs, trainData[:, 0]) - trainData[:, 1])**2.0)))
-
-    return(optimize.fmin(errFun, [300, 1.4], maxiter=2000, disp=0))
-
-def RZModel(coefs, rainRates) :
-    return(10.0 * numpy.log10(coefs[0] * ((rainRates + 0.001) ** coefs[1])))
-
-def RZBest(trainData) :
-    def errFun(coefs) :
-        return(numpy.sqrt(numpy.mean((RZModel(coefs, trainData[:, 1]) - trainData[:, 0])**2.0)))
-
-    return(optimize.fmin(errFun, [300, 1.4], maxiter=2000, disp=0))
-
-
-############################## Plotting #########################################
-def PlotCorr(obs, estimated, **kwargs) :
-    pylab.plot([0.0, obs.max()], [0.0, obs.max()], color='gray', linewidth=2.5)
-#    obsBins = MakeBins(obs.flatten(), OptimalBinSize(obs.flatten()))
-#    estBins = MakeBins(estimated.flatten(), OptimalBinSize(estimated.flatten()))
-#    (densVals, locs) = Hist2d(obs.flatten(), obsBins, estimated.flatten(), estBins)
-#    pylab.pcolor(obsBins, estBins, densVals, cmap=matplotlib.cm.get_cmap('Greys'))
-    pylab.scatter(obs.flatten(), estimated.flatten(), s=1.5, linewidths=0, c='black', alpha=0.75, hold=True, **kwargs)
-    pylab.xlabel('Observed Rainfall Rate [mm/hr]')
-    pylab.ylabel('Estimated Rainfall Rate [mm/hr]')
-    pylab.xlim((0.0, obs.max()))
-    pylab.ylim((0.0, obs.max()))
-
-def PlotZR(reflects, obs, estimated, **kwargs) :
-    pylab.scatter(reflects.flatten(), obs.flatten(), s = 3.0, linewidths=0, c='grey')
-    pylab.scatter(reflects.flatten(), estimated.flatten(), s = 0.3, linewidths=0, c='black', hold = True, **kwargs)
-
-    pylab.xlabel('Reflectivity [dBZ]')
-    pylab.ylabel('Rainfall Rate [mm/hr]')
-    pylab.xlim((reflects.min(), reflects.max()))
-    pylab.ylim((obs.min(), obs.max()))
-
-####################################################################################
-
-def ObtainModelInfo(dirLoc, subProj) :
-    modelList = glob.glob(os.sep.join([dirLoc, subProj, 'model_*.txt']))
-    modelCoefs = [ProcessModelInfo(filename) for filename in modelList]
-
-    coefNames = modelCoefs[0].keys()
-    coefNames.sort()
-
-    vals = []
-    for weight in modelCoefs :
-        vals.append([weight[coef] for coef in coefNames])
-
-    return((coefNames, numpy.array(vals)))
-
-
-
-#    print len(tempy), type(tempy[0])
-
-
-
-def ProcessModelInfo(filename) :
-    weights = {}
-    nodeName = None
-
-    for line in open(filename) :
-        line = line.strip()
-        if (line.startswith('Linear Node') or line.startswith('Sigmoid Node')) :
-	    nodeName = line.split(' ')[-1].strip()
-        elif  (line.startswith('Threshold') 
-		 or line.startswith('Node')
-		 or line.startswith('Attrib')) :
-            weights["%s-%s" % (nodeName, line.split()[-2])] = float(line.split()[-1])
-
-    return(weights)
-
-
-def ObtainARFFData(filename, columnIndxs, linesToSkip) :
-    return(numpy.loadtxt(filename, delimiter=',', skiprows=linesToSkip)[:, columnIndxs])
-    
-
-def ObtainResultInfo(dirLoc, subProj) :
-    resultsList = glob.glob(os.sep.join([dirLoc, subProj, 'results_*.csv']))
-    resultsList.sort()
-
-    skipMap = {'FullSet': 13,
-	       'SansWind': 11,
-	       'JustWind': 10,
-	       'Reflect': 8,
-	       'ZRBest': 0,
-	       'Shuffled': 13,
-	       'NWSZR': 0}
-
-
-    tempy = [ObtainARFFData(filename, numpy.array([-1, -2, -3]), skipMap[subProj]) for filename in resultsList]
-    return({'modelPredicts': numpy.array([aRow[:, 0] for aRow in tempy]),
-            'testObs': numpy.array([aRow[:, 1] for aRow in tempy]),
-            'reflectObs': numpy.array([aRow[:, 2] for aRow in tempy])})
-
-
-def SaveSubprojectModel(dirLoc, subProj) :
-    resultsList = glob.glob(os.sep.join([dirLoc, subProj, 'results_*.csv']))
-    resultsList.sort()
-
-    summaryInfo = {'rmse': [],
-		   'mae': [],
-		   'corr': [],
-		   'sse': [],
-		   'sae': []}
-    skipMap = {'FullSet': 13,
-	       'SansWind': 11,
-	       'JustWind': 10,
-	       'Reflect': 8,
-	       'ZRBest': 0,
-	       'Shuffled': 13,
-	       'NWSZR': 0}
-
-
-    resultInfo = ObtainResultInfo(dirLoc, subProj)
-    PlotCorr(resultInfo['testObs'], resultInfo['modelPredicts'])
-    pylab.title('Model/Obs Correlation Plot - Model: ' + subProj)
-    pylab.savefig(os.sep.join([dirLoc, "CorrPlot_" + subProj + ".eps"]))
-    pylab.clf()
-    print "   Saved Correlation Plot..."
-
-    PlotZR(resultInfo['reflectObs'], resultInfo['testObs'], resultInfo['modelPredicts'])
-    pylab.title('Model Comparison - Z-R Plane - ' + subProj)
-    pylab.savefig(os.sep.join([dirLoc, "ZRPlot_" + subProj + ".png"]))
-    pylab.clf()
-    print "   Save ZR Plot..."
-
-#    summaryInfo = DoSummaryInfo(resultInfo['testObs'], resultInfo['modelPredicts'])
-#    statNames = summaryInfo.keys()
-#    for statname in statNames :
-#        numpy.savetxt(os.sep.join([dirLoc, "summary_%s_%s.txt" % (statname, subProj)]), summaryInfo[statname])
-#        print "        Saved summary data for", statname
-
-
+from ProjectUtils import ObtainResultInfo, PlotCorr, PlotZR
 
 # Run this code if this script is executed like a program
 # instead of being loaded like a library file.
 if __name__ == '__main__':
-   projectName = 'ModelProject_Retry7/'
+   
+    from optparse import OptionParser	# Command-line parsing
 
-   resultInfo = {'FullSet': None, 'ZRBest': None}
+    parser = OptionParser()
+    parser.add_option("-r", "--run", dest="projectName", type="string",
+		      help="Use data from PROJECT run", metavar="PROJECT")
+    parser.add_option("-d", "--dir", dest="dataDir", type="string",
+		      help="Data exists at SRC", metavar="SRC", default=".")
+    parser.add_option("-m", "--models", dest="models", action="append", type="string",
+		      help="Create images for MODEL", metavar="MODEL")
 
-   resultInfo['FullSet'] = ObtainResultInfo(projectName, 'FullSet')
-   resultInfo['ZRBest'] = ObtainResultInfo(projectName, 'ZRBest')
+    (options, args) = parser.parse_args()
+    destDir = '.'
 
-   pylab.figure()
+    if options.projectName is None : parser.error("Missing PROJECT!")
+    if len(options.models) == 0 : print "WARNING: No models given!"
 
-   PlotCorr(resultInfo['ZRBest']['testObs'], resultInfo['ZRBest']['modelPredicts'])
-   pylab.title('Model/Obs Correlation Plot - Model: ZRBest', fontsize = 12)
+    pylab.figure()
 
-#   pylab.savefig("CorrZRBest_Raw.eps")
-   pylab.savefig("CorrZRBest_Raw.png", dpi = 400)
-   pylab.clf()
+    for model in options.models :
+        print "Model: ", model
+        resultInfo = ObtainResultInfo(os.sep.join([options.dataDir, options.projectName]), model)
 
-   PlotCorr(resultInfo['FullSet']['testObs'], resultInfo['FullSet']['modelPredicts'])
-   pylab.title('Model/Obs Correlation Plot - Model: FullSet', fontsize = 12)
+        ####### Plot Corr ##########
+        PlotCorr(resultInfo['testObs'], resultInfo['modelPredicts'], c='black')
+        pylab.title('Model/Obs Correlation Plot - Model: %s' % model, fontsize = 12)
 
-#   pylab.savefig("CorrFullSet_Raw.eps")
-   pylab.savefig("CorrFullSet_Raw.png", dpi = 400)
-   pylab.clf()
+        #pylab.savefig(os.sep.join([destDir, "Corr%s_Raw.eps" % model]))
+        pylab.savefig(os.sep.join([destDir, "Corr%s_Raw.png"  % model]), dpi = 400)
+        pylab.clf()
 
 
-   PlotZR(resultInfo['ZRBest']['reflectObs'], resultInfo['ZRBest']['testObs'], resultInfo['ZRBest']['modelPredicts'])
-   pylab.title('Model Comparison - Z-R Plane - Model: ZRBest', fontsize = 12)
+        ####### Plot ZR ###########
+        PlotZR(resultInfo['reflectObs'], resultInfo['testObs'], resultInfo['modelPredicts'])
+        pylab.title('Model Comparison - Z-R Plane - Model: %s' % model, fontsize = 12)
 
-#   pylab.savefig("ZRPlot_ZRBest_Raw.eps")
-   pylab.savefig("ZRPlot_ZRBest_Raw.png", dpi = 400)
-   pylab.clf()
-
-   PlotZR(resultInfo['FullSet']['reflectObs'], resultInfo['FullSet']['testObs'], resultInfo['FullSet']['modelPredicts'])
-   pylab.title('Model Comparison - Z-R Plane - Model: FullSet', fontsize = 12)
-
-#   pylab.savefig("ZRPlot_FullSet_Raw.eps")
-   pylab.savefig("ZRPlot_FullSet_Raw.png", dpi = 400)
-
-    
+        #pylab.savefig(os.sep.join([destDir, "ZRPlot_%s_Raw.eps" % model]))
+        pylab.savefig(os.sep.join([destDir, "ZRPlot_%s_Raw.png" % model]), dpi = 400)
+        pylab.clf()
+ 
 
